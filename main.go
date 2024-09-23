c4144bbadd6663eababe25d3db800d010923ea09
nans <nans.moll@ynov.com>
2024-09-23 11:33:15 +0200

    first commit 50mots

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
 
+	filePath := "C:/Users/nans-/GolandProjects/Hangman/Hangman/mots.txt"
+
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Println(err)
+		return
+
+	}
+
+	fmt.Println(string(data))
 }
