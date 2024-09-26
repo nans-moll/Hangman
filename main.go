d9eedfe230ea5a38705fb8e99edcbb83d54a40cd
nans <nans.moll@ynov.com>
2024-09-26 08:45:22 +0200

    test main

--- main.go
+++ main.go
@@ -1,20 +1,42 @@
 package main
 
 import (
+	"Hangman/game"
+	"Hangman/utils"
 	"fmt"
 	"os"
 )
 
 func main() {
-
-	filePath := "C:/Users/nans-/GolandProjects/Hangman/Hangman/mots.txt"
-
-	data, err := os.ReadFile(filePath)
-	if err != nil {
-		fmt.Println(err)
+	if len(os.Args) < 2 {
+		fmt.Println("ajouter un fichier existant avec")
 		return
+	}
+	wordsFile := os.Args[1]
+	words, err := utils.LoadWords(wordsFile)
+	if err != nil {
+		fmt.Printf("Erreur lors du chargement des mots: %v\n", err)
+		return
+	}
+	{
+		game.start(words)
 
+		fmt.Println("\nVous voulez ?:")
+		fmt.Println("							")
+		fmt.Println("1. New game")
+		fmt.Println("							")
+		fmt.Println("2. Exit")
+	}
+	var choice int
+	fmt.Print("Votre choix : ")
+	fmt.Scanln(&choice)
+
+	if choice == 2 {
+		fmt.Println("Merci d'avoir joué ! À bientôt.")
+		break
+	} else if choice != 1 {
+		fmt.Println("Choix non autorise. aurevoir.")
+		break
 	}
 
-	fmt.Println(string(data))
 }
